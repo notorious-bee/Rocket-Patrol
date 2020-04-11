a2505f92ce172adb2733a9cebcff104b4712c965
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-11 15:06:00 -0700

    commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,23 +3,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
+        // add object to existing scene
         scene.add.existing(this);
-        this.isFiring = false;
-    }
-
-    update() {
-        // left/right movement
-        if(this.isFiring) {
-            if(keyLEFT.isDown) {
-                this.x -= 12;
-            } else if(keyRIGHT.isDown && this.x <= 598) {
-                this.x += 12;
-            }
-        }
-        // fire button (NOT spacebar)
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
-            this.isFiring = true;
-        }
-        //
     }
 }
