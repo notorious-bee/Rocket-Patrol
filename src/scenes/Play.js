1fa2ebf2e7ad4a979694ded87c05999e7b309130
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-11 17:55:54 -0700

    Initial Commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -21,6 +21,9 @@ class Play extends Phaser.Scene {
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         // green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket');
     }
 
     update() {
