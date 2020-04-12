efff1007c9584268bd9fd810b78093505d6ce773
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-11 23:02:38 -0700

    Initial Commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -30,13 +30,13 @@ class Menu extends Phaser.Scene {
         let centerY = game.config.width/2;
         let textSpacer = 64;
 
-        this.add.text(centerX, centerY- textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0, 5);
-        this.add.text(centerX, centerY, 'Use ←→ arrows to move & (F) to Fire', menuConfig).setOrigin(0, 5);
+        this.add.text(centerX, centerY- textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY, 'Use ←→ arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
         this.add.text(centerX, centerY + textSpacer, 'Press ← for Easy or → for Hard', menuConfig).setOrigin(0, 5);
 
-        
+
         //this.scene.start("playScene");
     }
 }
