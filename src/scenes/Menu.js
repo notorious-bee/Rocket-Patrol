c19f4cf189d0b2148e3d599746dda729ab0e6e45
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-11 22:59:31 -0700

    Initial Commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,7 +11,32 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20, 20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+        // menu display
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        // show menu text
+        let centerX = game.config.width/2;
+        let centerY = game.config.width/2;
+        let textSpacer = 64;
+
+        this.add.text(centerX, centerY- textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0, 5);
+        this.add.text(centerX, centerY, 'Use ←→ arrows to move & (F) to Fire', menuConfig).setOrigin(0, 5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
+        this.add.text(centerX, centerY + textSpacer, 'Press ← for Easy or → for Hard', menuConfig).setOrigin(0, 5);
+
+        
+        //this.scene.start("playScene");
     }
 }
