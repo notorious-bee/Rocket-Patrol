6024a001aeaf971b6838a0fe4e315dc07908e571
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-11 18:58:49 -0700

    Initial Commit

--- prefabs/Spaceship.js
+++ prefabs/Spaceship.js
@@ -13,7 +13,11 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.x -= 3;
         // wraparound from left to right edge
         if(this.x <= 0-this.width) {
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    reset() {
+        this.x = game.config.width;
     }
 }
