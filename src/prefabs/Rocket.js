aee4b931da9ecfa0c738b8ae05783afbe27ae499
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-11 17:18:04 -0700

    initial commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,9 +1,9 @@
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
+    constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
 
-        // add object to existing scene (displayList, updateList)
+        // add to existing, displayList, updateList
         scene.add.existing(this);
         // track rocket's firing status
         this.isFiring = false;
