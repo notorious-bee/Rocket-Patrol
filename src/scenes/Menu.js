42f9d54a18cca1803aa5eba4bdaadb7eacb284b5
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-18 04:00:41 -0700

    Initial Commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -13,7 +13,6 @@ class Menu extends Phaser.Scene {
     create() {
         // menu display
         let menuConfig = {
-            TitleBackground: './assets/title background.png',
             fontFamily: 'Courier',
             fontSize: '28px',
             backgroundColor: '#F3B141',
@@ -27,6 +26,7 @@ class Menu extends Phaser.Scene {
         }
 
         // show menu text
+        this.load.image('title background', './assets/title background.png');
         let centerX = game.config.width/2;
         let centerY = game.config.height/2;
         let textSpacer = 64;
