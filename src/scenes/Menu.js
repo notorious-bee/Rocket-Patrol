5b6663219b6e901367847c77debb008b3be41d39
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-18 03:57:26 -0700

    Initial Commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,13 +8,12 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
-
-        this.load.image('title background', './assets/title background.png');
     }
 
     create() {
         // menu display
         let menuConfig = {
+            TitleBackground: './assets/title background.png',
             fontFamily: 'Courier',
             fontSize: '28px',
             backgroundColor: '#F3B141',
